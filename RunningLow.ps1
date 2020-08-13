fc872d3190246956db368fe236e7acbd5b340893
Darkseal <games@myst.it>
2020-08-13 18:09:40 +0200

    Added try/catch block to sending mail call.

--- RunningLow.ps1
+++ RunningLow.ps1
@@ -1,7 +1,7 @@
 ###
 #
 # ---------------------------------------------
-# RunningLow v1.1
+# RunningLow v1.1.1
 # ---------------------------------------------
 # A small Powershell script to check for low disk space and send e-mail to System Administrators
 #
@@ -59,8 +59,7 @@ $sep = ":";
 # if there are no $cur_hosts set, set the local computer as host. 
 if (!$hosts) { $hosts = $env:computername; }
 
-foreach ($cur_host in $hosts.split($sep))
-{
+foreach ($cur_host in $hosts.split($sep)) {
     # converts IP to hostNames
     if (($cur_host -As [IPAddress]) -As [Bool]) {
         $cur_host = [System.Net.Dns]::GetHostEntry($cur_host).HostName
@@ -85,76 +84,85 @@ foreach ($cur_host in $hosts.split($sep))
 		    }
 	    }
     }
-    Else { $drives_to_check = $volumes.split($sep) }
+    Else { 
+		$drives_to_check = $volumes.split($sep) 
+	}
 
-  foreach ($d in $drives_to_check) {
-	# Write-Host ("");
-	# Write-Host ("  Checking drive " + $d + " ...");
-	$disk = If ($cur_host -eq $env:computername) { Get-PSDrive $d }
-    Else { Invoke-Command -ComputerName $cur_host -ScriptBlock { Get-PSDrive $using:d } }
+    foreach ($d in $drives_to_check) {
+		# Write-Host ("");
+		# Write-Host ("  Checking drive " + $d + " ...");
+		$disk = If ($cur_host -eq $env:computername) { Get-PSDrive $d }
+		Else { Invoke-Command -ComputerName $cur_host -ScriptBlock { Get-PSDrive $using:d } }
 
-	if ($disk.Free -lt $minSize) {
-		Write-Host "  - [" -noNewLine
-        Write-Host "KO" -noNewLine -ForegroundColor Red
-        Write-Host "] " -noNewLine
-        Write-Host ("Drive " + $d + " has less than " + $minSize `
-			+ " bytes free (" + $disk.Free + " bytes - " + ($disk.Free/1MB).ToString(".00") + " GB)") -noNewLine
+		if ($disk.Free -lt $minSize) {
+			Write-Host "  - [" -noNewLine
+			Write-Host "KO" -noNewLine -ForegroundColor Red
+			Write-Host "] " -noNewLine
+			Write-Host ("Drive " + $d + " has less than " + $minSize `
+				+ " bytes free (" + $disk.Free + " bytes - " + ($disk.Free/1MB).ToString(".00") + " GB)") -noNewLine
          
-        if ($email_to) {
-            Write-Host(": sending e-mail..."); -noNewLine
+			if ($email_to) {
+				Write-Host(": sending e-mail..."); -noNewLine
 	
-		    $message = new-object Net.Mail.MailMessage;
-		    $message.From = $email_from;
-		    foreach ($to in $email_to.split($sep)) {
-			    $message.To.Add($to);
-		    }
-		    $message.Subject = 	("[RunningLow] WARNING: " + $cur_host + " drive " + $d);
-		    $message.Subject +=	(" has less than " + $minSize + " bytes free ");
-		    $message.Subject +=	("(" + $disk.Free + " bytes - "+ ($disk.Free/1GB).ToString(".00") +" GB)");
-		    $message.Body =		"Hello there, `r`n`r`n";
-		    $message.Body += 	"this is an automatic e-mail message ";
-		    $message.Body += 	"sent by RunningLow Powershell script ";
-		    $message.Body += 	("to inform you that " + $env:computername + " drive " + $d + " ");
-		    $message.Body += 	"is running low on free space. `r`n`r`n";
-		    $message.Body += 	"--------------------------------------------------------------";
-		    $message.Body +=	"`r`n";
-		    $message.Body += 	("Machine HostName: " + $env:computername + " `r`n");
-		    $message.Body += 	"Machine IP Address(es): ";
-		    $ipAddresses = Get-NetIPAddress -AddressFamily IPv4;
-		    foreach ($ip in $ipAddresses) {
-		        if ($ip.IPAddress -like "127.0.0.1") {
-			        continue;
-			    }
-		        $message.Body += ($ip.IPAddress + " ");
-		    }
-		    $message.Body += 	"`r`n";
-		    $message.Body += 	("Used space on drive " + $d + ": " + $disk.Used + " bytes. `r`n");
-		    $message.Body += 	("Free space on drive " + $d + ": " + $disk.Free + " bytes. `r`n");
-		    $message.Body += 	"--------------------------------------------------------------";
-		    $message.Body +=	"`r`n`r`n";
-		    $message.Body += 	"This warning will fire when the free space is lower ";
-		    $message.Body +=	("than " + $minSize + " bytes `r`n`r`n");
-		    $message.Body += 	"Sincerely, `r`n`r`n";
-		    $message.Body += 	"-- `r`n";
-		    $message.Body += 	"RunningLow`r`n";
-                    $message.Body +=        "http://www.ryadel.com/RunningLow";
+				$message = new-object Net.Mail.MailMessage;
+				$message.From = $email_from;
+				foreach ($to in $email_to.split($sep)) {
+					$message.To.Add($to);
+				}
+				$message.Subject = 	("[RunningLow] WARNING: " + $cur_host + " drive " + $d);
+				$message.Subject +=	(" has less than " + $minSize + " bytes free ");
+				$message.Subject +=	("(" + $disk.Free + " bytes - "+ ($disk.Free/1GB).ToString(".00") +" GB)");
+				$message.Body =		"Hello there, `r`n`r`n";
+				$message.Body += 	"this is an automatic e-mail message ";
+				$message.Body += 	"sent by RunningLow Powershell script ";
+				$message.Body += 	("to inform you that " + $env:computername + " drive " + $d + " ");
+				$message.Body += 	"is running low on free space. `r`n`r`n";
+				$message.Body += 	"--------------------------------------------------------------";
+				$message.Body +=	"`r`n";
+				$message.Body += 	("Machine HostName: " + $env:computername + " `r`n");
+				$message.Body += 	"Machine IP Address(es): ";
+				$ipAddresses = Get-NetIPAddress -AddressFamily IPv4;
+				foreach ($ip in $ipAddresses) {
+					if ($ip.IPAddress -like "127.0.0.1") {
+						continue;
+					}
+					$message.Body += ($ip.IPAddress + " ");
+				}
+				$message.Body += 	"`r`n";
+				$message.Body += 	("Used space on drive " + $d + ": " + $disk.Used + " bytes. `r`n");
+				$message.Body += 	("Free space on drive " + $d + ": " + $disk.Free + " bytes. `r`n");
+				$message.Body += 	"--------------------------------------------------------------";
+				$message.Body +=	"`r`n`r`n";
+				$message.Body += 	"This warning will fire when the free space is lower ";
+				$message.Body +=	("than " + $minSize + " bytes `r`n`r`n");
+				$message.Body += 	"Sincerely, `r`n`r`n";
+				$message.Body += 	"-- `r`n";
+				$message.Body += 	"RunningLow`r`n";
+						$message.Body +=        "http://www.ryadel.com/RunningLow";
 
-		    $smtp = new-object Net.Mail.SmtpClient($email_smtp_host, $email_smtp_port);
-		    $smtp.EnableSSL = $email_smtp_SSL;
-		    $smtp.Credentials = New-Object System.Net.NetworkCredential($email_username, $email_password);
-		    $smtp.send($message);
-		    $message.Dispose();
-		    Write-Host " E-Mail sent!" ; 
-        }
-        Else {
-            Write-Host(".");
-        }
+				$smtp = new-object Net.Mail.SmtpClient($email_smtp_host, $email_smtp_port);
+				$smtp.EnableSSL = $email_smtp_SSL;
+				$smtp.Credentials = New-Object System.Net.NetworkCredential($email_username, $email_password);
+
+				try {
+					$smtp.send($message);
+					Write-Host " E-Mail sent!" ;
+				}
+				catch {
+					Write-Host " Error sending E-Mail:" ;
+					Write-Host $_.Exception.Message
+				}
+				$message.Dispose();
+			}
+			Else {
+				Write-Host(".");
+			}
+		}
+		Else {
+			Write-Host "  - [" -noNewLine
+			Write-Host "OK" -noNewLine -ForegroundColor Green
+			Write-Host "] " -noNewLine
+			Write-Host ("Drive " + $d + " has more than " + $minSize + " bytes free: nothing to do.")
+		}
 	}
-	Else {
-		Write-Host "  - [" -noNewLine
-        Write-Host "OK" -noNewLine -ForegroundColor Green
-        Write-Host "] " -noNewLine
-		Write-Host ("Drive " + $d + " has more than " + $minSize + " bytes free: nothing to do.")
-	}
-  }
 }
